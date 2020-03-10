189ba1032775612f96b5f7690b623ec09b211f3c
vivekg13186 <vivek13186@gmail.com>
2020-03-10 21:14:25 +0100

    added active

--- index.js
+++ index.js
@@ -6,17 +6,17 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.post('/retrieveProcessApps', (req, res) => {
-    console.log(req.body.data);
+    //console.log(req.body.data);
     client.retrieveProcessApps(req.body['baseUrl'], req.body['username'], req.body['password'], (data) => { res.send(data); });
 });
 
 app.post('/retrieveToolkits', (req, res) => {
-    console.log(req.body.data);
+    //console.log(req.body.data);
     client.retrieveToolkits(req.body['baseUrl'], req.body['username'], req.body['password'], (data) => { res.send(data); });
 });
 
 app.post('/processAppsSetting', (req, res) => {
-    console.log(req.body.data);
+    //console.log(req.body.data);
     client.processAppsSetting(req.body['baseUrl'], req.body['username'], req.body['password'], req.body['snapshotId'], (data) => { res.send(data); });
 });
 
